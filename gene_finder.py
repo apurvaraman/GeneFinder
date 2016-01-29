e4b75f05f7de6ba637ef031808b13ff82dd4c05e
apurvaraman <apurva.raman@students.olin.edu>
2016-01-29 00:51:04 -0500

    Implemented Week 1 functions and doctests.

--- gene_finder.py
+++ gene_finder.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 YOUR HEADER COMMENT HERE
-
-@author: YOUR NAME HERE
+    What is the right content for a header comment? 
+@author: YOUR NAME HERE 
+    Me? I'm Apurva Raman
+    apurvaraman.github.com
 
 """
 
@@ -29,23 +31,60 @@ def get_complement(nucleotide):
     'T'
     >>> get_complement('C')
     'G'
+
+    checks if G maps to C
+    >>> get_complement('G')
+    'C'
+
+    checks if T maps to A
+    >>> get_complement('T')
+    'A'
+
+    checks if a non-nucleotide raises an error
+    >>> get_complement('Q')
+    Traceback (most recent call last):
+    ...
+    ValueError: A non-nucleotide character was entered.
+
     """
-    # TODO: implement this
+    if nucleotide == 'A':
+        return 'T'
+    elif nucleotide == 'T':
+        return 'A'
+    elif nucleotide == 'C':
+        return 'G'
+    elif nucleotide == 'G':
+        return 'C'
+    else:
+        raise ValueError("A non-nucleotide character was entered.")
+    #FINISHED: Implemented and tested
     pass
 
 
 def get_reverse_complement(dna):
     """ Computes the reverse complementary sequence of DNA for the specfied DNA
         sequence
-
         dna: a DNA sequence represented as a string
         returns: the reverse complementary DNA sequence represented as a string
+    
+    checks that strings with non-nucleotide characters in them raise an error
+    >>> get_reverse_complement("ATGCCCGQWERTY")
+    Traceback (most recent call last):
+    ...
+    ValueError: A non-nucleotide character was entered.
     >>> get_reverse_complement("ATGCCCGCTTT")
     'AAAGCGGGCAT'
     >>> get_reverse_complement("CCGCGTTCA")
     'TGAACGCGG'
     """
-    # TODO: implement this
+
+    build_complement = '' #initializes an empty string that the complements will be added to
+    for i in dna:
+        build_complement += get_complement(i)
+    reverse_complement_dna = build_complement[::-1]
+    return reverse_complement_dna
+
+    # FINISHED: IMPLEMENTED AND TESTED
     pass
 
 
@@ -57,12 +96,20 @@ def rest_of_ORF(dna):
 
         dna: a DNA sequence
         returns: the open reading frame represented as a string
+    checks that the function returns the whole string if there is no STOP codon
+    >>> rest_of_ORF('ATGAGA')
+    'ATGAGA'
     >>> rest_of_ORF("ATGTGAA")
     'ATG'
     >>> rest_of_ORF("ATGAGATAGG")
     'ATGAGA'
     """
-    # TODO: implement this
+    #The STOP codons are TAA, TAG, and TGA
+    for i in range(len(dna) / 3): #iterating through every frame
+        if dna[3*i:3*i+3] == 'TAG' or dna[3*i : 3*i+3] == 'TAA' or dna[3*i : 3*i+3] == 'TGA': #checking if one of the frames is the STOP codon
+            return dna[:3*i]
+    return dna
+    # FINISHED: IMPLEMENTED AND TESTED
     pass
 
 
@@ -76,10 +123,29 @@ def find_all_ORFs_oneframe(dna):
 
         dna: a DNA sequence
         returns: a list of non-nested ORFs
+    checks that if there is no START codon in dna, the result will be an empty list
+    >>> find_all_ORFs_oneframe("CATCATCATCATCAT")
+    []
+    checks that the resulting list starts with the START codon
+    >>> find_all_ORFs_oneframe("TTTCTTATTATGGTTTCTCAT")
+    ['ATGGTTTCTCAT']
+    checks that the resulting list ends with the STOP codon
+    >>> find_all_ORFs_oneframe("TTTCTTATTATGGTTTCTTAACAT")
+    ['ATGGTTTCT']
     >>> find_all_ORFs_oneframe("ATGCATGAATGTAGATAGATGTGCCC")
     ['ATGCATGAATGTAGA', 'ATGTGCCC']
     """
-    # TODO: implement this
+    ORFs = []
+    index = 0
+    while index < len(dna):
+        if dna[index: index + 3] == 'ATG':
+            new_ORF = len(rest_of_ORF(dna[index:]))
+            ORFs.append(rest_of_ORF(dna[index:]))
+            index += new_ORF
+        else:
+            index += 3
+    return ORFs
+    # FINISHED: IMPLEMENTED AND TESTED
     pass
 
 
@@ -92,11 +158,24 @@ def find_all_ORFs(dna):
 
         dna: a DNA sequence
         returns: a list of non-nested ORFs
-
-    >>> find_all_ORFs("ATGCATGAATGTAG")
+    checks that if there is no START codon in dna, the result will be an empty list
+    >>> find_all_ORFs('CATCATCATCATCAT')
+    []
+    checks that the resulting list ends with the STOP codon
+    >>> find_all_ORFs('ATGATGTAA')
+    ['ATGATG']
+    >>> find_all_ORFs('CTTTCTTATTATGGTTTCTCAT')
+    checks that an "out of frame" START codon does not result in an empty list
+    ['ATGGTTTCTCAT']
+    >>> find_all_ORFs('ATGCATGAATGTAG')
     ['ATGCATGAATGTAG', 'ATGAATGTAG', 'ATG']
     """
-    # TODO: implement this
+    ORFs_allframes = []
+    ORFs_allframes.extend(find_all_ORFs_oneframe(dna)) #first frame
+    ORFs_allframes.extend(find_all_ORFs_oneframe(dna[1:])) #second frame
+    ORFs_allframes.extend(find_all_ORFs_oneframe(dna[2:])) #third frame
+    return ORFs_allframes
+    # FINISHED: IMPLEMENTED AND TESTED
     pass
 
 
@@ -106,9 +185,17 @@ def find_all_ORFs_both_strands(dna):
 
         dna: a DNA sequence
         returns: a list of non-nested ORFs
-    >>> find_all_ORFs_both_strands("ATGCGAATGTAGCATCAAA")
+    checks that the result ignores nested ORFs and shows strands from both ORFs from the START to the STOP codon
+    >>> find_all_ORFs_both_strands('TTAATGATGATGCATCATTAA')
+    ['ATGATGATGCATCAT', 'ATGATGCATCATCAT']
+    >>> find_all_ORFs_both_strands('ATGCGAATGTAGCATCAAA')
     ['ATGCGAATG', 'ATGCTACATTCGCAT']
     """
+    ORFs_both_strands = []
+    ORFs_both_strands.extend(find_all_ORFs(dna)) #first strand
+    ORFs_both_strands.extend(find_all_ORFs(get_reverse_complement(dna))) #second strand
+    return ORFs_both_strands
+
     # TODO: implement this
     pass
 
@@ -163,4 +250,4 @@ def gene_finder(dna):
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    doctest.run_docstring_examples(find_all_ORFs_both_strands, globals(), verbose = True)
